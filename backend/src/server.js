e8ca84f3604a997bea7ba911985d8131c0582889
Dharmik Soni <sonidharmik21@gmail.com>
2026-07-16 17:33:38 +0530

    feat(backend): connect MongoDB with Express server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send(" AI Nexus Backend Running...");
+  res.send("AI Nexus Backend is Running ");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
